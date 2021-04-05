54b171a119892b5b2258635b8064cdb154f6d3e1
sue <sue@MacBook-Pro-de-sue.local>
2021-04-04 23:59:09 -0500

    Feat(Home): Fetching data in home, only log

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [response, setResponse] = useState(null); 
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const url = 'https://kmote.mx/devtest/';
+
+  useEffect(() => {
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const res = await fetch(url);
+            const json = await res.json();
+            setIsLoading(false);
+            if(isLoading) {
+              <p>please wait, i'm loading your data :D </p>
+            }
+            setResponse(json);
+        } catch {
+            setError(error);
+            if(error) {
+              <p>Ohh no!, 3312,3312: We have a problem houston</p>
+            }
+        }
+    };
+    fetchData();
+}, []);
+
+console.log(response)
+
   return (
     <>
     holi home :D
